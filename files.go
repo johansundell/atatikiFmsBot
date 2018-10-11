86b5ff96d1fbf0cd96e95f521627cb7d83542fb2
Johan Sundell <johan@pixpro.net>
2018-10-11 14:35:37 +0200

    Better message

--- files.go
+++ files.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/johansundell/atatikiFmsBot/fmsadmin"
 	//	"net/http"
@@ -17,15 +18,28 @@ func init() {
 			if err := s.Login(); err != nil {
 				return "", err
 			}
-			defer s.Logout()
 			files, err := s.GetFiles()
+			s.Logout()
 			if err != nil {
 				return "", err
 			}
-			msg := ""
+			msg := "Open files\n"
+			closed := "\nClosed files\n"
 			for _, r := range files.Files.Files {
-				msg += r.Filename + "\n"
+				//msg += r.Filename + "\n"
+				if r.Status == "NORMAL" {
+					msg += fmt.Sprintf("Id: %s, Name: %s, Clients: %d\n", r.ID, r.Filename, r.Clients)
+				} else {
+					closed += "Id: " + r.ID + ", " + r.Filename + "\n"
+				}
 			}
+			msg += closed + "\n"
+
+			msg += fmt.Sprintf("FileMaker Pro: %d\n", files.FmproCount)
+			msg += fmt.Sprintf("FileMaker Go: %d\n", files.FmgoCount)
+			msg += fmt.Sprintf("FileMaker WebDirect: %d\n", files.FmwebdCount)
+			msg += fmt.Sprintf("FileMaker Data API: %d\n", files.FmdapiCount)
+			msg += fmt.Sprintf("Connected but not counted: %d\n", files.FmmiscCount)
 			return msg, nil
 		}
 		return "", nil
